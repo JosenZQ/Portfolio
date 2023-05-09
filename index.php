5b7a71996638b29ab5aedd191eb23004673b7420
JosenZQ <josenzamoraquesada02@gmail.com>
2023-05-08 22:17:25 -0600

    Cambios en estilos

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/img/portfolio.png">
     <link rel="stylesheet" href="./assets/css/styles.css">
     <title>Portafolio</title>
 </head>
+
 <body>
     <section>
-        <button class="open-button"><div class="button-icon"></div></button>
+        <button class="open-button">
+            <div class="button-icon"></div>
+        </button>
 
-        <div class="header-title"><h1 class="portfolio-title">Portafolio</h1></div>
+        <div class="header-title">
+            <h1 class="portfolio-title">Portafolio</h1>
+        </div>
         <!--<hr class="line">-->
-        <div class="header-subtitle"><h3 class="portfolio-subtitle">Josen Zamora</h3></div>
+        <div class="header-subtitle">
+            <h3 class="portfolio-subtitle">Josen Zamora</h3>
+        </div>
 
         <div class="link-container">
             <a href="https://github.com/JosenZQ"><img class="img-link" src="./assets/img/github.png" alt="GitHub"></a>
@@ -26,16 +35,6 @@
         <div class="wave wave3"></div>
         <div class="wave wave4"></div>
     </section>
-
-    <div class="side-bar">
-        <nav>
-            <a href="#">Pantalla 01</a>
-            <a href="#">Pantalla 02</a>
-            <a href="#">Pantalla 03</a>
-            <a href="#">Pantalla 04</a>
-            <a href="#">Pantalla 05</a>
-        </nav>
-    </div>
 
     <div>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
@@ -54,5 +53,7 @@
         Tempora, eos! Quo autem veniam, quia dolorem voluptas illum ut ipsa<br>
         a soluta numquam vero modi culpa porro eaque tenetur doloremque libero?<br>
     </div>
+
 </body>
+
 </html>
