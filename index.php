15e8dd3ead18331896f3b9820e9044c4a5e220a9
JosenZQ <josenzamoraquesada02@gmail.com>
2023-05-08 20:17:40 -0600

    Commit inicial

--- index.php
+++ index.php
@@ -1,6 +1,58 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <title>Portafolio</title>
+</head>
+<body>
+    <section>
+        <button class="open-button"><div class="button-icon"></div></button>
 
-    print("Hola Mundo");
+        <div class="header-title"><h1 class="portfolio-title">Portafolio</h1></div>
+        <!--<hr class="line">-->
+        <div class="header-subtitle"><h3 class="portfolio-subtitle">Josen Zamora</h3></div>
 
+        <div class="link-container">
+            <a href="https://github.com/JosenZQ"><img class="img-link" src="./assets/img/github.png" alt="GitHub"></a>
+            <a href="https://www.linkedin.com/in/david-zamora-72044a265/"><img class="img-link" src="./assets/img/linkedin-logo.png" alt="Linkedn"></a>
+            <a href="https://twitter.com/SenZQ26"><img class="img-link" src="./assets/img/twitter-sign.png" alt="Twitter"></a>
+        </div>
 
-?>
+        <div class="wave wave1"></div>
+        <div class="wave wave2"></div>
+        <div class="wave wave3"></div>
+        <div class="wave wave4"></div>
+    </section>
+
+    <div class="side-bar">
+        <nav>
+            <a href="#">Pantalla 01</a>
+            <a href="#">Pantalla 02</a>
+            <a href="#">Pantalla 03</a>
+            <a href="#">Pantalla 04</a>
+            <a href="#">Pantalla 05</a>
+        </nav>
+    </div>
+
+    <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
+        Tempora, eos! Quo autem veniam, quia dolorem voluptas illum ut ipsa<br>
+        a soluta numquam vero modi culpa porro eaque tenetur doloremque libero?<br>
+
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
+        Tempora, eos! Quo autem veniam, quia dolorem voluptas illum ut ipsa<br>
+        a soluta numquam vero modi culpa porro eaque tenetur doloremque libero?<br>
+
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
+        Tempora, eos! Quo autem veniam, quia dolorem voluptas illum ut ipsa<br>
+        a soluta numquam vero modi culpa porro eaque tenetur doloremque libero?<br>
+
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.<br>
+        Tempora, eos! Quo autem veniam, quia dolorem voluptas illum ut ipsa<br>
+        a soluta numquam vero modi culpa porro eaque tenetur doloremque libero?<br>
+    </div>
+</body>
+</html>
